39541e7027cdf430f4c020cb55251cbd5c7d35f3
rosston <ross.brandes@gmail.com>
2017-01-29 11:13:29 -0500

    Add minified build and source maps.

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,46 @@
 'use strict'
 
-module.exports = {
-  entry: './src/incremental-hyperscript.js',
-  output: {
-    path: './dist',
-    filename: 'incremental-hyperscript.js',
-    library: 'IncrementalHyperscript',
-    libraryTarget: 'umd'
-  },
-  externals: {
-    'incremental-dom': {
-      commonjs: 'incremental-dom',
-      commonjs2: 'incremental-dom',
-      amd: 'incremental-dom',
-      root: 'IncrementalDOM'
-    }
+var webpack = require('webpack')
+
+module.exports = function (env) {
+  var isMinifiedBuild = (env === 'min')
+
+  var filename = (isMinifiedBuild)
+    ? 'incremental-hyperscript.min.js'
+    : 'incremental-hyperscript.js'
+
+  var plugins
+  if (isMinifiedBuild) {
+    plugins = [
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true
+      })
+    ]
+  } else {
+    plugins = []
+  }
+
+  return {
+    entry: './src/incremental-hyperscript.js',
+    devtool: 'source-map',
+    output: {
+      path: './dist',
+      filename: filename,
+      library: 'IncrementalHyperscript',
+      libraryTarget: 'umd'
+    },
+    externals: {
+      'incremental-dom': {
+        commonjs: 'incremental-dom',
+        commonjs2: 'incremental-dom',
+        amd: 'incremental-dom',
+        root: 'IncrementalDOM'
+      }
+    },
+    plugins: plugins
   }
 }
